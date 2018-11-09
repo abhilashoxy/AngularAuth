cdca384c290d2558e021bec5182059796230c3f8
abhilashoxy@gmail.com <abhilashoxy@gmail.com>
2018-11-09 19:04:35 +0530

    added login and home and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './Home/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './Home/navigation/navigation.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Login',      component:LoginComponent },
+  
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes
+     
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
